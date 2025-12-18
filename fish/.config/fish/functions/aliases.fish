60af593e48ffbf4d88165785aca57933fabe4611
riceman2000 <alex4222000@gmail.com>
2025-12-18 10:44:26 -0500

    New alias

--- fish/.config/fish/functions/aliases.fish
+++ fish/.config/fish/functions/aliases.fish
@@ -20,12 +20,15 @@ abbr -a ai "aichat"
 
 # Skim - fuzzy finding
 export SKIM_DEFAULT_COMMAND="fd --type file --hidden --follow --exclude .git --color=always"
+export SKIM_DIRONLY_COMMAND="fd --type dir --hidden --follow --exclude .git --color=always"
+export SKIM_RIPGREP_COMMAND="rg --hidden --color=always --line-number {}"
 export SKIM_DEFAULT_OPTIONS="--ansi"
 
-alias rsk "sk -i -c 'rg --hidden --color=always --line-number {}' --preview '~/scripts/preview.sh {}'" # Because this is used later it needs to be an alias
+alias rsk "sk -i -c '$SKIM_RIPGREP_COMMAND' --preview '~/scripts/preview.sh {}'" # Because this is used later it needs to be an alias
 abbr -a rvsk "rsk | awk -F: '{print \$1 \" +\" \$2}' | xargs nvim" 
-abbr -a bsk "sk --bind 'enter:execute(bat {})+abort' || true"
 abbr -a vsk "sk --bind 'enter:execute(nvim {})+abort' || true"
+abbr -a bsk "sk --bind 'enter:execute(bat {})+abort' || true"
+abbr -a csk "cd \$(sk -c \"$SKIM_DIRONLY_COMMAND\")"
 
 # Git
 abbr -a fap "git fetch && git pull"
